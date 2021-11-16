23cf65288baec04dc054cf25d2a9f47e2eab0317
Oshan Fernando <ofernando@wiley.com>
2021-11-16 19:37:29 +0530

    Initial commit

---
+++ test/cca-aws.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CcaAws from '../lib/cca-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cca-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CcaAws.CcaAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
